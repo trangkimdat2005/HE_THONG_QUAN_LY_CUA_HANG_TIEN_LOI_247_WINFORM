4caf74b311057b3bc24dfb4b5df8cabc02807e5f
taplamcode <tranthanhngan.29.05.2005@gmail.com>
2025-11-20 23:28:58 +0700

    report

--- HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WINFORM/Views/Forms/Reports/frmSalesReport.Designer.cs
+++ HE_THONG_QUAN_LY_CUA_HANG_TIEN_LOI_247_WINFORM/Views/Forms/Reports/frmSalesReport.Designer.cs
@@ -17,12 +17,18 @@
 
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.pnlTop = new System.Windows.Forms.Panel();
             this.lblTitle = new System.Windows.Forms.Label();
             this.pnlControls = new System.Windows.Forms.Panel();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.dtpToDate = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpFromDate = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
             this.pnlGrid = new System.Windows.Forms.Panel();
             this.dgvReports = new System.Windows.Forms.DataGridView();
             this.colSTT = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -30,12 +36,6 @@
             this.colPeriod = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colTotalRevenue = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colAction = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.dtpToDate = new System.Windows.Forms.DateTimePicker();
-            this.label3 = new System.Windows.Forms.Label();
-            this.dtpFromDate = new System.Windows.Forms.DateTimePicker();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lblSearch = new System.Windows.Forms.Label();
-            this.txtSearch = new System.Windows.Forms.TextBox();
             this.pnlTop.SuspendLayout();
             this.pnlControls.SuspendLayout();
             this.pnlGrid.SuspendLayout();
@@ -67,107 +67,47 @@
             // 
             this.pnlControls.BackColor = System.Drawing.Color.White;
             this.pnlControls.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlControls.Controls.Add(this.btnRefresh);
+            this.pnlControls.Controls.Add(this.btnFilter);
             this.pnlControls.Controls.Add(this.dtpToDate);
             this.pnlControls.Controls.Add(this.label3);
             this.pnlControls.Controls.Add(this.dtpFromDate);
             this.pnlControls.Controls.Add(this.label2);
-            this.pnlControls.Controls.Add(this.txtSearch);
-            this.pnlControls.Controls.Add(this.lblSearch);
             this.pnlControls.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlControls.Location = new System.Drawing.Point(0, 60);
             this.pnlControls.Name = "pnlControls";
             this.pnlControls.Size = new System.Drawing.Size(1000, 60);
             this.pnlControls.TabIndex = 1;
             // 
-            // pnlGrid
+            // btnRefresh
             // 
-            this.pnlGrid.Controls.Add(this.dgvReports);
-            this.pnlGrid.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlGrid.Location = new System.Drawing.Point(0, 120);
-            this.pnlGrid.Name = "pnlGrid";
-            this.pnlGrid.Padding = new System.Windows.Forms.Padding(10);
-            this.pnlGrid.Size = new System.Drawing.Size(1000, 480);
-            this.pnlGrid.TabIndex = 2;
+            this.btnRefresh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btnRefresh.FlatAppearance.BorderSize = 0;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnRefresh.ForeColor = System.Drawing.Color.White;
+            this.btnRefresh.Location = new System.Drawing.Point(490, 19);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(100, 33);
+            this.btnRefresh.TabIndex = 11;
+            this.btnRefresh.Text = "Làm mới";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
-            // dgvReports
+            // btnFilter
             // 
-            this.dgvReports.AllowUserToAddRows = false;
-            this.dgvReports.AllowUserToDeleteRows = false;
-            this.dgvReports.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvReports.BackgroundColor = System.Drawing.Color.White;
-            this.dgvReports.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dgvReports.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
-            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
-            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvReports.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
-            this.dgvReports.ColumnHeadersHeight = 40;
-            this.dgvReports.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.colSTT,
-            this.colReportId,
-            this.colPeriod,
-            this.colTotalRevenue,
-            this.colAction});
-            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle9.Font = new System.Drawing.Font("Segoe UI", 9.75F);
-            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
-            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvReports.DefaultCellStyle = dataGridViewCellStyle9;
-            this.dgvReports.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvReports.EnableHeadersVisualStyles = false;
-            this.dgvReports.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
-            this.dgvReports.Location = new System.Drawing.Point(10, 10);
-            this.dgvReports.MultiSelect = false;
-            this.dgvReports.Name = "dgvReports";
-            this.dgvReports.ReadOnly = true;
-            this.dgvReports.RowHeadersVisible = false;
-            this.dgvReports.RowTemplate.Height = 40;
-            this.dgvReports.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvReports.Size = new System.Drawing.Size(980, 460);
-            this.dgvReports.TabIndex = 0;
-            this.dgvReports.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvReports_CellClick);
-            // 
-            // colSTT
-            // 
-            this.colSTT.HeaderText = "STT";
-            this.colSTT.Name = "colSTT";
-            this.colSTT.ReadOnly = true;
-            // 
-            // colReportId
-            // 
-            this.colReportId.HeaderText = "Mã Báo Cáo";
-            this.colReportId.Name = "colReportId";
-            this.colReportId.ReadOnly = true;
-            // 
-            // colPeriod
-            // 
-            this.colPeriod.HeaderText = "Kỳ Báo Cáo";
-            this.colPeriod.Name = "colPeriod";
-            this.colPeriod.ReadOnly = true;
-            // 
-            // colTotalRevenue
-            // 
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
-            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
-            this.colTotalRevenue.DefaultCellStyle = dataGridViewCellStyle8;
-            this.colTotalRevenue.HeaderText = "Tổng Doanh Thu";
-            this.colTotalRevenue.Name = "colTotalRevenue";
-            this.colTotalRevenue.ReadOnly = true;
-            // 
-            // colAction
-            // 
-            this.colAction.HeaderText = "Thao tác";
-            this.colAction.Name = "colAction";
-            this.colAction.ReadOnly = true;
-            this.colAction.Text = "📥";
-            this.colAction.UseColumnTextForButtonValue = true;
+            this.btnFilter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnFilter.FlatAppearance.BorderSize = 0;
+            this.btnFilter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFilter.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnFilter.ForeColor = System.Drawing.Color.White;
+            this.btnFilter.Location = new System.Drawing.Point(380, 19);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(100, 33);
+            this.btnFilter.TabIndex = 10;
+            this.btnFilter.Text = "Lọc dữ liệu";
+            this.btnFilter.UseVisualStyleBackColor = false;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
             // 
             // dtpToDate
             // 
@@ -197,6 +137,7 @@
             this.dtpFromDate.Name = "dtpFromDate";
             this.dtpFromDate.Size = new System.Drawing.Size(150, 25);
             this.dtpFromDate.TabIndex = 7;
+            this.dtpFromDate.Value = new System.DateTime(1999, 11, 24, 0, 0, 0, 0);
             // 
             // label2
             // 
@@ -209,25 +150,97 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "Từ ngày";
             // 
-            // lblSearch
+            // pnlGrid
             // 
-            this.lblSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblSearch.AutoSize = true;
-            this.lblSearch.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.lblSearch.Location = new System.Drawing.Point(520, 19);
-            this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(52, 19);
-            this.lblSearch.TabIndex = 2;
-            this.lblSearch.Text = "Search:";
+            this.pnlGrid.Controls.Add(this.dgvReports);
+            this.pnlGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGrid.Location = new System.Drawing.Point(0, 120);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Padding = new System.Windows.Forms.Padding(10);
+            this.pnlGrid.Size = new System.Drawing.Size(1000, 480);
+            this.pnlGrid.TabIndex = 2;
             // 
-            // txtSearch
+            // dgvReports
             // 
-            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtSearch.Location = new System.Drawing.Point(580, 16);
-            this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(250, 25);
-            this.txtSearch.TabIndex = 3;
+            this.dgvReports.AllowUserToAddRows = false;
+            this.dgvReports.AllowUserToDeleteRows = false;
+            this.dgvReports.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvReports.BackgroundColor = System.Drawing.Color.White;
+            this.dgvReports.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvReports.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvReports.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvReports.ColumnHeadersHeight = 40;
+            this.dgvReports.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colSTT,
+            this.colReportId,
+            this.colPeriod,
+            this.colTotalRevenue,
+            this.colAction});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvReports.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvReports.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvReports.EnableHeadersVisualStyles = false;
+            this.dgvReports.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.dgvReports.Location = new System.Drawing.Point(10, 10);
+            this.dgvReports.MultiSelect = false;
+            this.dgvReports.Name = "dgvReports";
+            this.dgvReports.ReadOnly = true;
+            this.dgvReports.RowHeadersVisible = false;
+            this.dgvReports.RowTemplate.Height = 40;
+            this.dgvReports.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvReports.Size = new System.Drawing.Size(980, 460);
+            this.dgvReports.TabIndex = 0;
+            this.dgvReports.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvReports_CellClick);
+            // 
+            // colSTT
+            // 
+            this.colSTT.FillWeight = 30F;
+            this.colSTT.HeaderText = "STT";
+            this.colSTT.Name = "colSTT";
+            this.colSTT.ReadOnly = true;
+            // 
+            // colReportId
+            // 
+            this.colReportId.HeaderText = "Mã Báo Cáo";
+            this.colReportId.Name = "colReportId";
+            this.colReportId.ReadOnly = true;
+            // 
+            // colPeriod
+            // 
+            this.colPeriod.HeaderText = "Kỳ Báo Cáo";
+            this.colPeriod.Name = "colPeriod";
+            this.colPeriod.ReadOnly = true;
+            // 
+            // colTotalRevenue
+            // 
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+            this.colTotalRevenue.DefaultCellStyle = dataGridViewCellStyle2;
+            this.colTotalRevenue.HeaderText = "Tổng Doanh Thu";
+            this.colTotalRevenue.Name = "colTotalRevenue";
+            this.colTotalRevenue.ReadOnly = true;
+            // 
+            // colAction
+            // 
+            this.colAction.FillWeight = 50F;
+            this.colAction.HeaderText = "Thao tác";
+            this.colAction.Name = "colAction";
+            this.colAction.ReadOnly = true;
+            this.colAction.Text = "📥";
+            this.colAction.UseColumnTextForButtonValue = true;
             // 
             // frmSalesReport
             // 
@@ -241,6 +254,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmSalesReport";
             this.Text = "Báo cáo doanh thu";
+            this.Load += new System.EventHandler(this.frmSalesReport_Load);
             this.pnlTop.ResumeLayout(false);
             this.pnlTop.PerformLayout();
             this.pnlControls.ResumeLayout(false);
@@ -258,16 +272,16 @@
         private System.Windows.Forms.Panel pnlControls;
         private System.Windows.Forms.Panel pnlGrid;
         private System.Windows.Forms.DataGridView dgvReports;
+        private System.Windows.Forms.DateTimePicker dtpToDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpFromDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnRefresh;
         private System.Windows.Forms.DataGridViewTextBoxColumn colSTT;
         private System.Windows.Forms.DataGridViewTextBoxColumn colReportId;
         private System.Windows.Forms.DataGridViewTextBoxColumn colPeriod;
         private System.Windows.Forms.DataGridViewTextBoxColumn colTotalRevenue;
         private System.Windows.Forms.DataGridViewButtonColumn colAction;
-        private System.Windows.Forms.DateTimePicker dtpToDate;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DateTimePicker dtpFromDate;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtSearch;
-        private System.Windows.Forms.Label lblSearch;
     }
 }
